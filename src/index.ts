d91f1827ee3f17ca23bfda822295582ce6493ec1
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-09-17 22:06:53 -0300

    Create update and updateContentById functions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import fs from 'fs'
+import { v4 as uuid } from 'uuid';
 const DB_FILE_PATH = './src/db'
 
 console.log('[CRUD iniciado]')
 
 interface Todo {
+    id: string
     date: string
     content: string
     done: boolean
 }
 
-function create(content: string) {
+function create(content: string): Todo {
     const todo: Todo = {
+        id: uuid(),
         date: new Date().toISOString(),
         content,
         done: false,
@@ -21,7 +24,28 @@ function create(content: string) {
     ]
 
     fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2))
-    return content
+    return todo
+}
+
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+    let updatedTodo;
+    const todos = read()
+    todos.forEach(todo => {
+        const isToUpdate = todo.id === id
+        if (isToUpdate) {
+            updatedTodo = Object.assign(todo, partialTodo)
+        }
+    })
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({ todos }, null, 2))
+
+    if (!updatedTodo) throw new Error('Todo not founded with the provided Id')
+
+    return updatedTodo
+}
+
+function updateContentById(id: string, content: string) {
+    return update(id, { content })
 }
 
 function read(): Array<Todo> {
@@ -41,6 +65,7 @@ function cleanDb() {
 // SIMULATION
 cleanDb()
 create('Primeira TODO')
-create('Segunda TODO')
+const segundaTodo = create('Segunda TODO')
 create('Teeerceira TODO')
+updateContentById(segundaTodo.id, 'Segunda TODO ATULAIZADA!')
 console.log(read())
