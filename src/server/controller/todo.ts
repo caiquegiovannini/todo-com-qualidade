2a5bde771c8f20f574af9d895c90abacb04c650a
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-11-16 22:58:17 -0300

    Add server pagination

--- src/server/controller/todo.ts
+++ src/server/controller/todo.ts
@@ -5,6 +5,8 @@ function get(_: NextApiRequest, res: NextApiResponse) {
   const output = todoRepository.get();
 
   res.status(200).json({
+    total: output.total,
+    pages: output.pages,
     todos: output.todos,
   });
 }
