29aadaf897f51a21dc7714fa93e404e797eccc82
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-11-27 15:51:59 -0300

    Create filterTodosByContent

--- src/ui/controller/todo.ts
+++ src/ui/controller/todo.ts
@@ -9,6 +9,20 @@ function get(params: TodoControllerGetParams) {
   return todoRepository.get({ page: params.page, limit: params.limit });
 }
 
+function filterTodosByContent(
+  search: string,
+  todos: Array<{ content: string }>
+) {
+  const searchNormalized = search.toLowerCase();
+  const homeTodos = todos.filter((todo) => {
+    const contentNormalized = todo.content.toLowerCase();
+    return contentNormalized.includes(searchNormalized);
+  });
+
+  return homeTodos;
+}
+
 export const todoController = {
   get,
+  filterTodosByContent,
 };
