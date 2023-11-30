8c4eb18e2f814de7c3cd70204a0ef72377343b53
Caique Giovannini <49538313+caiquegiovannini@users.noreply.github.com>
2023-11-29 23:25:26 -0300

    Create todo toggle-done handler

---
+++ pages/api/todos/[id]/toggle-done.ts
@@ -0,0 +1,14 @@
+import { todoController } from "@server/controller/todo";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === "PUT") {
+    todoController.toggleDone(request, response);
+    return;
+  }
+
+  response.status(405).json({ error: { message: "Methodot allowed" } });
+}
